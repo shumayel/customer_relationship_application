46ef5e56c5f9a82c142c301f71bc95b7ae0b7c08
shumayel.khan@mail.utoronto.ca <shumayel.khan@mail.utoronto.ca>
2015-06-13 19:13:03 -0400

    Initial files from Mina - first commit

---
+++ contact.rb
@@ -0,0 +1,14 @@
+class Contact
+  attr_accessor :first_name, :last_name, :email, :notes
+  attr_reader :id
+
+  def initialize(id, first_name, last_name, email, notes)
+    @id = id
+    @first_name = first_name.capitalize
+    @last_name = last_name.capitalize
+    @email = email
+    @notes = notes
+  end
+end
+
+    Status API Training Shop Blog About Help
